73f7bd6fd53c836d10d1789bcad607a7bfb80498
Ammithasri <ammithasriakilan1995@gmail.com>
2026-07-18 22:48:18 +0530

    Add files via upload

---
+++ Day01/dataType.js
@@ -0,0 +1,28 @@
+//String
+
+var firstName = 'Ammitha';
+var companyName = 'XYZ';
+
+console.log(firstName);
+console.log(companyName);
+
+//Number
+var mobileNumber = 9876543210;
+console.log(mobileNumber);
+
+//Boolean
+var isAutomation = true;
+console.log(isAutomation);
+
+//undefined
+var hasPlaywright
+console.log(hasPlaywright);
+
+console.log(typeof firstName, typeof companyName);
+console.log(typeof companyName);
+console.log(typeof mobileNumber);
+console.log(typeof isAutomation);
+console.log(typeof hasPlaywright)
+
+
+
