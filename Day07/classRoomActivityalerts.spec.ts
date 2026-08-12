1a08d23f0aaa880825d09f2df88443bfaf863b39
Ammithasri <ammithasriakilan1995@gmail.com>
2026-08-12 16:20:40 +0530

    Delete Day07/classRoomActivityalerts.spec.ts

--- Day07/classRoomActivityalerts.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from '@playwright/test'
-
-// login to leaftaps, create a lead
-test('handle alerts', async ({ page }) => {
-    await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm")
-    const frame = page.frameLocator(`#iframeResult`)
-
-})
